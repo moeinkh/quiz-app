399014984c59e116e3054eab99fd26965f4b8a62
moein <moeinkhorami99@gmail.com>
2022-04-26 16:37:19 +0430

    add form for quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Question, Category
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question, Category, Answer
+from .form import AnswerForm
+from django.contrib import messages
+from django.urls import reverse
 
 # Create your views here.
 def home(request, id=None):
@@ -14,7 +17,20 @@ def list_quiz(request, id):
 
     if id:
         category = get_object_or_404(Category, id=id)
-        question = question.filter(category=category)
+        question = question.filter(category=category).order_by('?')[:1]
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data['is_right']:
+                print(form.cleaned_data['is_right'])
+                messages.success(request, 'جواب درست است.')
+                return redirect(reverse('list_quiz', args=[id]))
+            else:
+                print(form.cleaned_data)
+                messages.warning(request, 'جواب نادرست می باشد.')  
+    else:
+        form = AnswerForm()              
 
     return render(request, 'quiz/quizes.html', {
         'question': question,
